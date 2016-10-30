652512ecb4b9cd47d784432411ef44468aa68f13
Melissa Jean Clark <melissa@melissajclark.ca>
2016-10-29 21:02:09 -0400

    removed references to automattic, added my details

--- footer.php
+++ footer.php
@@ -14,11 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
-		</div><!-- .site-info -->
+			<p><?php printf( esc_html__( 'Website by: %1$s by %2$s.', '_s' ), '_s', '<a href="https://melissajclark.ca">Melissa Jean Clark</a>' ); ?></p>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
