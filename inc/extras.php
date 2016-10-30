dba6d1cf0fa7987d887411b478242ae707d0127f
Melissa Jean Clark <melissa@melissajclark.ca>
2016-10-29 21:07:26 -0400

    removed _s functions, added editor styles and filter to lower Yoast SEO

--- inc/extras.php
+++ inc/extras.php
@@ -7,33 +7,23 @@
  * @package _s
  */
 
-/**
- * Adds custom classes to the array of body classes.
+ /**
  *
- * @param array $classes Classes for the body element.
- * @return array
- */
-function _s_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
-	}
+ * Add an editor stylesheet to the WordPress admin
+ * See: https://developer.wordpress.org/reference/functions/add_editor_style/
+ *
+ **/
 
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
-	}
-
-	return $classes;
-}
-add_filter( 'body_class', '_s_body_classes' );
+ function _s_add_editor_styles() {
+ 	add_editor_style( 'editor-style.css' );
+ }
+ add_action( 'admin_init', '_s_add_editor_styles' );
 
 /**
- * Add a pingback url auto-discovery header for singularly identifiable articles.
- */
-function _s_pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		echo '<link rel="pingback" href="', bloginfo( 'pingback_url' ), '">';
-	}
-}
-add_action( 'wp_head', '_s_pingback_header' );
+*
+* Filter Yoast SEO's settings, so it is below custom metaboxes.
+* https://wordpress.org/support/topic/plugin-wordpress-seo-by-yoast-position-seo-meta-box-priority-above-custom-meta-boxes/
+*
+*/
+
+add_filter( 'wpseo_metabox_prio', function() { return 'low';});
