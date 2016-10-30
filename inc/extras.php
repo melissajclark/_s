be5a43bc9da9ff4541dbc49ae3fb9a5688c583bf
Melissa Jean Clark <melissa@melissajclark.ca>
2016-10-29 22:31:21 -0400

    added function for typekit fonts

--- inc/extras.php
+++ inc/extras.php
@@ -27,3 +27,21 @@
 */
 
 add_filter( 'wpseo_metabox_prio', function() { return 'low';});
+
+
+/**
+* TypeKit Fonts
+*
+*/
+
+// function _s_typekit() {
+//     wp_enqueue_script( '_s_typekit', '//use.typekit.net/-----.js');
+// }
+// add_action( 'wp_enqueue_scripts', '_s_typekit' );
+
+// function _s_typekit_inline() {
+//   if ( wp_script_is( '_s_typekit', 'done' ) ) { ?>
+  	 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+// <?php }
+// }
+// add_action( 'wp_head', '_s_typekit_inline' );
