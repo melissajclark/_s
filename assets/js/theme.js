41fedcc46844ae411dff790d1d11ee1a0c2dae04
Melissa Jean Clark <melissa@melissajclark.ca>
2017-01-26 14:29:19 -0500

    added Object Fit polyfill and enqueued scripts

---
+++ assets/js/theme.js
@@ -0,0 +1,14 @@
+jQuery(function ($) { 
+	
+	/**
+	 *
+	 * Uncomment to turn on Object Fit Polyfill
+	 *
+	 */
+	
+	// objectFitImages() 
+
+
+	// More theme-specfic code here
+
+});
--- functions.php
+++ functions.php
@@ -104,9 +104,37 @@ add_action( 'widgets_init', '_s_widgets_init' );
 function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 
+		'_s-skip-link-focus-fix', // handle (name)
+		get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', // file location
+		array(), // dependencies
+		'20151215', // version number
+		true // load in footer 
+	);
 
-	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 
+		'_s-navigation', // handle (name)
+		get_template_directory_uri() . '/assets/js/navigation.js', // file location
+		array(), // dependencies
+		'20151215', // version number
+		true // load in footer 
+	);
+
+	wp_enqueue_script( 
+		'object-fit-polyfill', // handle (name)
+		get_template_directory_uri() . '/assets/js/object-fit-polyfill.js', // file location
+		array(), // dependencies
+		null, // version number
+		true // load in footer 
+	);
+
+	wp_enqueue_script( 
+		'theme', // handle (name)
+		get_template_directory_uri() . '/assets/js/theme.js', // file location
+		array('jquery'), // dependencies
+		null, // version number
+		true // load in footer 
+	);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
