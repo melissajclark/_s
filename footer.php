d1db79e836bfe4a896d86928ae2b08644b41baf9
Melissa Jean Clark <melissa@melissajclark.ca>
2017-09-30 22:36:48 -0400

    Footer - added copyright symbol, current year, site name

--- footer.php
+++ footer.php
@@ -15,6 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<?php _s_social_media_links(); ?>
+		<p>&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?></p>
 		<p><?php printf( esc_html__( 'Website by: %1$s by %2$s.', '_s' ), '_s', '<a href="https://melissajclark.ca">Melissa Jean Clark</a>' ); ?></p>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
