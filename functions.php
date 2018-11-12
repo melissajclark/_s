07f8ec812faeb57d6ecdb75b5b7035e8ac9c8391
Melissa Jean Clark <melissa@melissajclark.ca>
2018-11-12 10:48:16 -0500

    Functions.php - add support for custom logo

--- functions.php
+++ functions.php
@@ -61,10 +61,25 @@ function _s_setup() {
 	) );
 
 	// Set up the WordPress core custom background feature.
-	// add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
-		// 'default-color' => 'ffffff',
-		// 'default-image' => '',
-	// ) ) );
+	add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	/**
+	 * Add support for core custom logo.
+	 *
+	 * @link https://codex.wordpress.org/Theme_Logo
+	 */
+	add_theme_support( 'custom-logo', array(
+		'height'      => 250,
+		'width'       => 250,
+		'flex-width'  => true,
+		'flex-height' => true,
+	) );
 }
 endif;
 add_action( 'after_setup_theme', '_s_setup' );
