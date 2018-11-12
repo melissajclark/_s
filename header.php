7b8b8dccc90f25fe7bfdc0840ae44239d48fcd3a
Melissa Jean Clark <melissa@melissajclark.ca>
2018-11-12 10:14:47 -0500

    Header.php - reformat wp_nav_menu function

--- header.php
+++ header.php
@@ -40,7 +40,15 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			
+			<?php wp_nav_menu( 
+				array( 
+					'theme_location' 	=> 'primary', 
+					'menu_id' 			=> 'primary-menu',
+					'menu_class' 		=> 'menu',
+					'container'			=> '', 
+				) 
+			); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
