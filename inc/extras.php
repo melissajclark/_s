ec975d46c4f4dcb33611333889a698d8edca260b
Melissa Clark <melissa@melissajclark.ca>
2021-04-18 18:21:03 -0400

    add function for body classes

--- inc/extras.php
+++ inc/extras.php
@@ -1,11 +1,30 @@
 <?php
 /**
- * Custom functions that act independently of the theme templates.
- *
- * Eventually, some of the functionality here could be replaced by core features.
- *
+ * Custom functions for this theme.
+ * *
  * @package _s
  */
+
+ /**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function _s_body_classes( $classes ) {
+	// Adds a class of hfeed to non-singular pages.
+	if ( ! is_singular() ) {
+		$classes[] = 'hfeed';
+	}
+
+	// Adds a class of no-sidebar when there is no sidebar present.
+	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+		$classes[] = 'no-sidebar';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', '_s_body_classes' );
 
 /**
  * Enqueue login page scripts and styles.
