0715f3d390823c41e74f21fb560fed543c081888
Melissa Clark <melissa@melissajclark.ca>
2021-04-18 14:22:25 -0400

    Create Block editor JS file, move JS plugins to plugins.js file

---
+++ assets/js/block-editor.js
@@ -0,0 +1,39 @@
+wp.domReady( () => {
+	
+	// remove unecessary block options
+	// ------------------------------
+
+	// buttons
+	wp.blocks.unregisterBlockStyle( 'core/button', 'default' );
+	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
+
+	// images
+	wp.blocks.unregisterBlockStyle( 'core/image', 'default' );
+	wp.blocks.unregisterBlockStyle( 'core/image', 'rounded' );
+
+	// separator
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'wide' );
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'dots' );
+	wp.blocks.unregisterBlockStyle( 'core/separator', 'default' );
+
+	// quote
+	wp.blocks.unregisterBlockStyle( 'core/quote', 'default' );
+	wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
+
+	// remove unecessary block types
+	// ----------------------------
+	wp.blocks.unregisterBlockType( 'core/pullquote' );
+	wp.blocks.unregisterBlockType( 'core/verse' );
+	wp.blocks.unregisterBlockType( 'core/search' );
+	wp.blocks.unregisterBlockType( 'core/cover' );
+	wp.blocks.unregisterBlockType( 'core/more' );
+	wp.blocks.unregisterBlockType( 'core/gallery' );
+	wp.blocks.unregisterBlockType( 'core/navigation' );
+	wp.blocks.unregisterBlockType( 'core/nextpage' );
+	wp.blocks.unregisterBlockType( 'core/tag-cloud' );
+	wp.blocks.unregisterBlockType( 'core/social-links' );
+	wp.blocks.unregisterBlockType( 'core/calendar' );
+	wp.blocks.unregisterBlockType( 'core/latest-comments' );
+
+} );
