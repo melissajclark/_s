16f34a7bc32b0cacea4b0fae01062d9f8893a707
Melissa Clark <melissa@melissajclark.ca>
2021-04-18 17:35:08 -0400

    remove h1 in header

--- header.php
+++ header.php
@@ -23,13 +23,7 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif; ?>
+			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
